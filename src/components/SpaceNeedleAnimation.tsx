c8860067215038b4e19c9e2bb6b0e7840c5556b6
Zach Romano <zacharyr24@hotmail.com>
2019-11-10 23:00:52 -0800

    Work content and added footer

--- src/components/SpaceNeedleAnimation.tsx
+++ src/components/SpaceNeedleAnimation.tsx
@@ -3,10 +3,10 @@ import SVG from "react-inlinesvg";
 import SpaceNeedle from "./space-needle.svg";
 
 const SpaceNeedleAnimation: React.FC = () => {
-  return(
-    <div className={'space-needle-animation'}>
-      <SVG src={SpaceNeedle}/>
-    </div>
-  );
+    return (
+        <div className={'space-needle-animation'}>
+            <SVG src={SpaceNeedle}/>
+        </div>
+    );
 };
 export {SpaceNeedleAnimation}
