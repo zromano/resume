2c9b57ff7c80966f97b52a323776ef9f2b63ef88
Zach Romano <zacharyr24@hotmail.com>
2020-09-19 17:31:03 -0700

    Added Presentations

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,12 @@ const App: React.FC = () => {
                                     and modern web development.
                                 </p>
                                 <p>
-                                    I began my engineering journey at the University of Illinois - Urbana/Champaign where
-                                    I graduated in 2017 with a degree in aerospace engineering. Aerospace led me to Boeing,
-                                    where I started out as a systems engineer, but later transitioned to a software engineering
+                                    I began my engineering journey at the University of Illinois - Urbana/Champaign
+                                    where
+                                    I graduated in 2017 with a degree in aerospace engineering. Aerospace led me to
+                                    Boeing,
+                                    where I started out as a systems engineer, but later transitioned to a software
+                                    engineering
                                     role. In addition to working at Boeing, I also completed my Masters in Computer
                                     Science from Johns Hopkins University in August 2020.
                                 </p>
@@ -38,10 +41,43 @@ const App: React.FC = () => {
                             <img src={process.env.PUBLIC_URL + '/zach.jpeg'} alt='Zach'/>
                         </div>
                     </div>
-
                     <hr/>
                     <div className="work">
                         <JobHistory/>
+                    </div>
+                    <hr/>
+                    <div className="presentations">
+                        <h1>Presentations</h1>
+                        <div className="presentations__item">
+                            <h4>Capture the Flag</h4>
+                            <p>
+                                Watch me demonstrate how to find the vulnerabilities in my capture the
+                                flag activity
+                            </p>
+                            <iframe
+                                title="Cature the Flag Presentation"
+                                className="presentations__item_video"
+                                src="https://www.youtube.com/embed/3UJVxCiLxSE?start=450"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            />
+                        </div>
+                        <div className="presentations__item">
+                            <h4>Paper Presentation:</h4>
+                            <p>
+                                Watch me present the paper "Cross-VM Side Channels and Their Use to
+                                Extract Private Keys"
+                            </p>
+                            <iframe
+                                title="Paper Presentation"
+                                className="presentations__item_video"
+                                src="https://www.youtube.com/embed/BEEizbdQFkk"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            />
+                        </div>
                     </div>
                     <hr/>
                     <div className="projects">
